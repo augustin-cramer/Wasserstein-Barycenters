e66cfa28cfd49b8417439eb29fadd235fe2c5b0c
Augustin Cramer <augustin.cramer@qantev.com>
2024-01-09 17:47:02 +0100

    add read_off

--- notebooks/augustin/plotting_utils.py
+++ notebooks/augustin/plotting_utils.py
@@ -19,3 +19,11 @@ def plot_mesh(arr_verts, arr_faces, show_axis=True):
         fig.update_scenes(xaxis_visible=False, yaxis_visible=False,zaxis_visible=False )
 
     fig.show()
+    
+def read_off(file):
+    if 'OFF' != file.readline().strip():
+        raise('Not a valid OFF header')
+    n_verts, n_faces, n_dontknow = tuple([int(s) for s in file.readline().strip().split(' ')])
+    verts = [[float(s) for s in file.readline().strip().split(' ')] for i_vert in range(n_verts)]
+    faces = [[int(s) for s in file.readline().strip().split(' ')][1:] for i_face in range(n_faces)]
+    return verts, faces
