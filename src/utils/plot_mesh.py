ead491f9d3e3998c51c844548fd2a99f9ae208ac
Augustin Cramer <augustin.cramer@qantev.com>
2024-01-17 18:30:12 +0100

    add a plotting function

---
+++ src/utils/plot_mesh.py
@@ -0,0 +1,19 @@
+import plotly.graph_objects as go
+
+def plot_mesh(vertices, faces, show_axis=True, **kwargs):
+    fig = go.Figure(data=[
+        go.Mesh3d(
+            x=vertices[:,0],
+            y=vertices[:,1],
+            z=vertices[:,2],
+            i=faces[:,0],
+            j=faces[:,1],
+            k=faces[:,2],
+            **kwargs
+        ),
+    ])
+    
+    if not show_axis:
+        fig.update_scenes(xaxis_visible=False, yaxis_visible=False,zaxis_visible=False )
+
+    fig.show()
