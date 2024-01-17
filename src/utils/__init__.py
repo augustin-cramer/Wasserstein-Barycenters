4ddfc65bedb216e1fed2dd85a22eae6731e41231
Augustin Cramer <augustin.cramer@qantev.com>
2024-01-17 18:30:47 +0100

    update with new funcions

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -2,3 +2,5 @@ from .gaussian_blur_kernel import gaussian_blur_kernel
 from .mesh_to_point_cloud import mesh_to_point_cloud
 from .read_off import read_off
 from .rescale import rescale
+from .point_cloud_to_mesh import point_cloud_to_mesh
+from .plot_mesh import plot_mesh
