9dc1d019dc4e39b250f5a0ff16f06a58e7c153de
Augustin Cramer <augustin.cramer@qantev.com>
2024-01-19 18:06:45 +0100

    remove personal files

--- notebooks/augustin/toolbox/rescale.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def rescale(array, inf, sup):
-    """
-    Rescale an array to a given interval.
-    """
-    a = np.min(array)
-    b = np.max(array)
-    return (array - a) / (b - a) * (sup - inf) + inf
